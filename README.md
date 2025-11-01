007a1bd454264961d8bede1983b8d8cf300addd2
Joao.Morais <97574443+jpmmat@users.noreply.github.com>
2025-11-01 13:52:20 +0000

    Detalhando

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Aula13
 Calculadora IMC simples
+Saber o  peso ideal observando parametros regualdores da saúde segundo Organização Mundial da Saude(OMS)
---
+++ imc.js
@@ -0,0 +1,18 @@
+let peso = parseFloat(prompt("Digite seu peso em kg. Ex.: 80,0"));
+let altura = parseFloat(prompt("Digite sua altura em metros. Ex.: 1.75 "));
+//Calculo do Imc
+let imc = peso/(altura*altura);
+
+// condicoes de avaliacao do peso
+if(imc < 18.5){
+  console.log(`Seu IMC é ${imc}. Você esta abaixo do nivel ideal de peso`)
+}else if(imc>18.5 && imc<= 24.9){
+  console.log(`Seu IMC é ${imc}. Você está com peso ideal`)
+}else if(imc>24.9&& imc<=29.9){
+  console.log(`Seu IMC é ${imc}.Você está com peso um pouco acima do  normal` )
+}else if(imc>29.9&& imc <= 39.9){
+  console.log(`Seu IMC é ${imc}.Você está em estado de obesidade`)
+}
+else if(imc>39.9){
+  console.log(`Seu IMC é ${imc}.Você está em estado de obesidade grave`)
+};
